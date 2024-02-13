43bee87f81a2d4a3c25f35224749f31e407b3c20
ntimba <chancy@ntimba.com>
2024-02-14 00:02:56 +0100

    This commit adds the logic enabling the creation of a group (#4).

--- src/Controller/AddTrickGroupController.php
+++ src/Controller/AddTrickGroupController.php
@@ -2,15 +2,67 @@
 
 namespace App\Controller;
 
+use App\Entity\TrickGroup;
+use App\Repository\TrickGroupRepository;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AddTrickGroupController extends AbstractController
 {
-    #[Route('/add/trick/group', name: 'app_add_trick_group')]
-    public function index(): Response
+    private TrickGroupRepository $groupRepository;
+    private SluggerInterface $slugger;
+
+    public function __construct(TrickGroupRepository $groupRepository, SluggerInterface $slugger)
     {
+        $this->groupRepository = $groupRepository;
+        $this->slugger = $slugger;
+    }
+    
+    /**
+     * This method adds a new group to the database if it doesn't exist.
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/add/trick/group', name: 'app_add_trick_group')]
+    public function index(Request $request, EntityManagerInterface $manager ): Response
+    {
+        $trickGroup = new TrickGroup();
+
+        if( 
+            $request->isMethod('POST') &&
+            $request->request->has('_group_name') &&
+            $request->request->has('_group_escription') 
+            )
+        {
+            $groupName = $request->request->get('_group_name');
+            $groupDescription = $request->request->get('_group_escription');
+
+            $trickGroup->setName( $groupName );
+            $trickGroup->setDescription( $groupDescription );
+
+            $groupSlug = $this->slugger->slug( $groupName );
+
+            $groupExist = $this->groupRepository->findOneBy(['slug' => $groupSlug] );
+            if( !$groupExist ){
+                try{
+                    $manager->persist($trickGroup);
+                    $manager->flush();
+                } catch( UniqueConstraintViolationException $e ) {
+                    $this->addFlash('error', "Impossible d'jouter ce groupe");
+                    return $this->redirectToRoute('app_add_trick_group');
+                }
+            }else{
+                $this->addFlash('warning', "Ce groupe existe déjà");
+            }   
+        }
+        
         return $this->render('add_trick_group/index.html.twig', [
             'controller_name' => 'AddTrickGroupController',
         ]);
---
+++ src/Repository/TrickGroupRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\TrickGroup;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<TrickGroup>
+ *
+ * @method TrickGroup|null find($id, $lockMode = null, $lockVersion = null)
+ * @method TrickGroup|null findOneBy(array $criteria, array $orderBy = null)
+ * @method TrickGroup[]    findAll()
+ * @method TrickGroup[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TrickGroupRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TrickGroup::class);
+    }
+}
